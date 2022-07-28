b56d520be04fbcb27b40f5886bd38d12fe5f869b
zbihari <zbihari@szintezis-net.hu>
2022-07-28 15:51:48 +0200

    Init project

---
+++ src/main/java/hu/byhi/dnd/calulator/CalulatorApplication.java
@@ -0,0 +1,13 @@
+package hu.byhi.dnd.calulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/byhi/dnd/calulator/CalulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.dnd.calulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
